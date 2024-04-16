1897feb23d31e9be81b835cb4b8e1ab6a7c795d4
Blair Bourque <cosmomail12@gmail.com>
2024-04-16 12:49:53 -0500

    new colors and logo added

--- GUI/NewGui.py
+++ GUI/NewGui.py
@@ -10,6 +10,7 @@ set_appearance_mode("dark")
 #image names
 image1 = CTkImage(Image.open("C:/Users/baili/Downloads/CSC-132-Final-Pi-Project/GUI/Images/side-image.jpg"), size=(500, 500))
 image2 = CTkImage(Image.open("C:/Users/baili/Downloads/CSC-132-Final-Pi-Project/GUI/Images/text.png"),size = (500, 150))
+image3 = CTkImage(Image.open("C:/Users/baili/Downloads/CSC-132-Final-Pi-Project/GUI/Images/logo.png"),size = (250, 150))
 
 #frame1 setup & contents
 frame1 = CTkFrame(master=GUI, width=500,height=500,bg_color="transparent")
@@ -24,7 +25,7 @@ placed_image2 = CTkLabel(master=frame2, image=image2, text="")
 placed_image2.grid(row=2,column=0,columnspan=3,rowspan=2)
 
 #frame3 setup & contents
-frame3 = CTkFrame(master=GUI,width=250,height=200)
+frame3 = CTkFrame(master=GUI,width=250,height=250,fg_color="#81B0E0")
 frame3.grid(row=2,column=4,columnspan=2,rowspan=2)
 dropdown = CTkComboBox(master=frame3, values=["Small","Medium","Large"])
 dropdown.grid(column=0,row=1,sticky=S)
@@ -32,7 +33,7 @@ frame1_label = CTkLabel(master=frame3,text="Size",font=("Arial", 20),text_color=
 frame1_label.grid(column=0,row=0,sticky=N)
 
 #frame4 setup & contents
-frame4 = CTkFrame(master=GUI,width=250,height=200)
+frame4 = CTkFrame(master=GUI,width=250,height=250,fg_color="#81B0E0")
 frame4.grid(row=2,column=2,columnspan=2,rowspan=2)
 frame4_label = CTkLabel(master=frame4,text="Thickness",font=("Arial", 20),text_color="White")
 frame4_label.grid(column=0,row=0,sticky=N)
@@ -40,7 +41,7 @@ entry = CTkEntry(master=frame4)
 entry.grid(column=0,row=1,sticky=S)
 
 #frame5 setup & contents
-frame5 = CTkFrame(master=GUI,width=250,height=200,bg_color="#81B0E0")
+frame5 = CTkFrame(master=GUI,width=250,height=250,fg_color="#81B0E0")
 frame5.grid(row=4,column=2,columnspan=2,rowspan=2)
 dropdown = CTkComboBox(master=frame5, values=["1","2","3",'4'])
 dropdown.grid(column=0,row=1,sticky=S)
@@ -48,7 +49,9 @@ frame5_label = CTkLabel(master=frame5,text="Slices Amount",font=("Arial", 20),te
 frame5_label.grid(column=0,row=0,sticky=N)
 
 #frame6 setup & contents
-frame6 = CTkFrame(master=GUI,width=250,height=200)
+frame6 = CTkFrame(master=GUI,width=250,height=250)
 frame6.grid(row=4,column=4,columnspan=2,rowspan=2)
+placed_image3 = CTkLabel(master=frame6, image=image3, text="")
+placed_image3.grid()
 
 GUI.mainloop()
