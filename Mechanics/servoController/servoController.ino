735862d636169ea46cb4252947dd26a7b6ade4a3
Brandon Fortes <55511864+MrFortez@users.noreply.github.com>
2024-04-15 20:14:20 -0500

    Update servoController.ino

--- Mechanics/servoController/servoController.ino
+++ Mechanics/servoController/servoController.ino
@@ -1,17 +1,32 @@
-#include <Servo.h>
+#include <Wire.h>
 
-Servo test;
+#define SLAVE_ADDRESS 0x08 // Arduino I2C slave address
+#define ARRAY_SIZE 5       // Size of the array to receive
 
+int receivedData[ARRAY_SIZE]; // Array to store received data
 
 void setup() {
-  // put your setup code here, to run once:
-  test.attach(9);
+  Wire.begin(SLAVE_ADDRESS); // Initialize I2C communication as a slave
+  Wire.onReceive(receiveEvent); // Call receiveEvent() function when receiving data
+  Serial.begin(9600); // Initialize serial communication
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  test.write(0);
-  delay(5000);
-  test.write(250);
-  delay(5000);
+  delay(100);
+}
+
+void receiveEvent(int numBytes) {
+  int i = 0;
+  while (Wire.available() && i < ARRAY_SIZE) {
+    receivedData[i] = Wire.read(); // Read the incoming data byte into the array
+    i++;
+  }
+  
+  // Print received array
+  Serial.print("Received Array: ");
+  for (int j = 0; j < ARRAY_SIZE; j++) {
+    Serial.print(receivedData[j]);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
